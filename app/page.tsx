3b62f41b78c808111c943b7c96dbd8765a66c029
Nikesh222005 <nikeshkumar222005@gmail.com>
2024-11-13 13:36:45 +0530

    init

---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Home() {
+  return (
+    <div className="w-screen flex items-center justify-center h-screen"><span className="flex justify-center border border-neutral-800 p-20 rounded-xl text-2xl">Nikesh Kumar</span></div>
+  );
+}
